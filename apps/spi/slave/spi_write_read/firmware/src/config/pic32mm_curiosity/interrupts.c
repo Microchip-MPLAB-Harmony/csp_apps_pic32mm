c3472eec5a89306ee02645f386647ed8f10ad5f5
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-26 14:30:43 +0530

    [SPI] Regenerated demo for spi_slave_spi_write_read_pic32mm_curiosity
    MH3-77967

--- apps/spi/slave/spi_write_read/firmware/src/config/pic32mm_curiosity/interrupts.c
+++ apps/spi/slave/spi_write_read/firmware/src/config/pic32mm_curiosity/interrupts.c
@@ -60,14 +60,23 @@
 // *****************************************************************************
 
 
-void CHANGE_NOTICE_B_InterruptHandler( void );
-void SPI2_ERR_InterruptHandler( void );
-void SPI2_TX_InterruptHandler( void );
-void SPI2_RX_InterruptHandler( void );
-
-
-
 /* All the handlers are defined here.  Each will call its PLIB-specific function. */
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Interrupt Vector declarations
+// *****************************************************************************
+// *****************************************************************************
+void CHANGE_NOTICE_B_Handler (void);
+void SPI2_ERR_Handler (void);
+void SPI2_TX_Handler (void);
+void SPI2_RX_Handler (void);
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Interrupt Vector definitions
+// *****************************************************************************
+// *****************************************************************************
 void __ISR(_CHANGE_NOTICE_B_VECTOR, ipl1SOFT) CHANGE_NOTICE_B_Handler (void)
 {
     CHANGE_NOTICE_B_InterruptHandler();
