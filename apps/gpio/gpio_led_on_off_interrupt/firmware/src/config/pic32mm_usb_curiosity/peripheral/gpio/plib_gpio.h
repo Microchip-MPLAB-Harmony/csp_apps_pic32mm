4a18bd34caeb76a2b61bfc4f6dce08fa42c7773b
Pranjal Das <pranjal.das@microchip.com>
2024-10-23 14:42:17 +0530

    [GPIO] Regenerated demo for gpio__gpio_led_on_off_interrupt__firmware__pic32mm_usb_curiosity.X
    MH3-100682

--- apps/gpio/gpio_led_on_off_interrupt/firmware/src/config/pic32mm_usb_curiosity/peripheral/gpio/plib_gpio.h
+++ apps/gpio/gpio_led_on_off_interrupt/firmware/src/config/pic32mm_usb_curiosity/peripheral/gpio/plib_gpio.h
@@ -69,6 +69,7 @@
 #define LED1_OutputEnable()      (TRISDCLR = (1<<3))
 #define LED1_InputEnable()       (TRISDSET = (1<<3))
 #define LED1_Get()               ((PORTD >> 3) & 0x1)
+#define LED1_GetLatch()          ((LATD >> 3) & 0x1)
 #define LED1_PIN                  GPIO_PIN_RD3
 
 /*** Macros for SWITCH3 pin ***/
@@ -78,6 +79,7 @@
 #define SWITCH3_OutputEnable()      (TRISCCLR = (1<<4))
 #define SWITCH3_InputEnable()       (TRISCSET = (1<<4))
 #define SWITCH3_Get()               ((PORTC >> 4) & 0x1)
+#define SWITCH3_GetLatch()          ((LATC >> 4) & 0x1)
 #define SWITCH3_PIN                  GPIO_PIN_RC4
 #define SWITCH3_InterruptEnable()   (CNEN0CSET = (1<<4))
 #define SWITCH3_InterruptDisable()  (CNEN0CCLR = (1<<4))
