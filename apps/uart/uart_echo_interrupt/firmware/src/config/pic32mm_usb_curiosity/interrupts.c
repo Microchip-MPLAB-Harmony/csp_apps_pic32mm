7dbb0c2f4e3062fb371e4683b18fc08c9e136ca9
Pranjal Das <pranjal.das@microchip.com>
2024-10-23 14:42:22 +0530

    [UART] Regenerated demo for uart__uart_echo_interrupt__firmware__pic32mm_usb_curiosity.X
    MH3-100714

--- apps/uart/uart_echo_interrupt/firmware/src/config/pic32mm_usb_curiosity/interrupts.c
+++ apps/uart/uart_echo_interrupt/firmware/src/config/pic32mm_usb_curiosity/interrupts.c
@@ -48,9 +48,9 @@
 // Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
-
 #include "interrupts.h"
 #include "definitions.h"
+
 
 
 // *****************************************************************************
@@ -76,17 +76,17 @@ void UART3_ERR_Handler (void);
 // Section: System Interrupt Vector definitions
 // *****************************************************************************
 // *****************************************************************************
-void __ISR(_UART3_RX_VECTOR, ipl1SOFT) UART3_RX_Handler (void)
+void __attribute__((used)) __ISR(_UART3_RX_VECTOR, ipl1SOFT) UART3_RX_Handler (void)
 {
     UART3_RX_InterruptHandler();
 }
 
-void __ISR(_UART3_TX_VECTOR, ipl1SOFT) UART3_TX_Handler (void)
+void __attribute__((used)) __ISR(_UART3_TX_VECTOR, ipl1SOFT) UART3_TX_Handler (void)
 {
     UART3_TX_InterruptHandler();
 }
 
-void __ISR(_UART3_ERR_VECTOR, ipl1SOFT) UART3_ERR_Handler (void)
+void __attribute__((used)) __ISR(_UART3_ERR_VECTOR, ipl1SOFT) UART3_ERR_Handler (void)
 {
     UART3_ERR_InterruptHandler();
 }
