95655cee98336bfed3d0f0a90f377c44ab67bc81
Pranjal Das <pranjal.das@microchip.com>
2024-10-23 14:39:13 +0530

    [I2C] Regenerated demo for i2c__slave__i2c_eeprom_emulation__firmware__pic32mm_usb_curiosity.X
    MH3-100686

--- apps/i2c/slave/i2c_eeprom_emulation/firmware/src/config/pic32mm_usb_curiosity/peripheral/i2c/slave/plib_i2c1_slave.c
+++ apps/i2c/slave/i2c_eeprom_emulation/firmware/src/config/pic32mm_usb_curiosity/peripheral/i2c/slave/plib_i2c1_slave.c
@@ -51,6 +51,7 @@
 #include "plib_i2c1_slave.h"
 #include "interrupts.h"
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Global Data
@@ -59,14 +60,14 @@
 #define I2C1_SLAVE_DATA_SETUP_TIME_CORE_TIMER_CNTS          1
 #define I2C1_SLAVE_RISE_TIME_CORE_TIMER_CNTS                3
 
-volatile static I2C_SLAVE_OBJ i2c1Obj;
+volatile static I2C_SLAVE_OBJ i2c1SlaveObj;
 
 void I2C1_Initialize(void)
 {
     /* Turn off the I2C module */
     I2C1CONCLR = _I2C1CON_ON_MASK;
 
-    I2C1CON = (_I2C1CON_STREN_MASK | _I2C1CON_PCIE_MASK );
+    I2C1CONSET = (_I2C1CON_STREN_MASK );
 
     I2C1ADD = 0x54;
 
@@ -84,10 +85,11 @@ void I2C1_Initialize(void)
     /* Enable the I2C Bus collision interrupt */
     IEC2SET = _IEC2_I2C1BCIE_MASK;
 
-    i2c1Obj.callback = NULL;
+    i2c1SlaveObj.callback = NULL;
 
     /* Turn on the I2C module */
     I2C1CONSET = _I2C1CON_ON_MASK;
+
 }
 
 static void I2C1_RiseAndSetupTime(uint8_t sdaState)
@@ -116,30 +118,35 @@ static void I2C1_TransferSM(void)
 {
     uint32_t i2c_addr;
     uint8_t sdaValue = 0U;
-    uintptr_t context = i2c1Obj.context;
+    uintptr_t context = i2c1SlaveObj.context;
 
     /* ACK the slave interrupt */
     IFS2CLR = _IFS2_I2C1SIF_MASK;
 
     if ((I2C1STAT & _I2C1STAT_P_MASK) != 0U)
     {
-        if (i2c1Obj.callback != NULL)
+        I2C1CONCLR = _I2C1CON_PCIE_MASK;
+
+        if (i2c1SlaveObj.callback != NULL)
         {
-            (void)i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_STOP_BIT_RECEIVED, context);
+            (void)i2c1SlaveObj.callback(I2C_SLAVE_TRANSFER_EVENT_STOP_BIT_RECEIVED, context);
         }
+
     }
     else if ((I2C1STAT & _I2C1STAT_D_A_MASK) == 0U)
     {
+        I2C1CONSET = _I2C1CON_PCIE_MASK;
+
         if ((I2C1STAT & _I2C1STAT_RBF_MASK) != 0U)
         {
             /* Received I2C address must be read out */
             i2c_addr = I2C1RCV;
             (void)i2c_addr;
 
-            if (i2c1Obj.callback != NULL)
+            if (i2c1SlaveObj.callback != NULL)
             {
                 /* Notify that a address match event has occurred */
-               (void)i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_ADDR_MATCH, context);
+               (void)i2c1SlaveObj.callback(I2C_SLAVE_TRANSFER_EVENT_ADDR_MATCH, context);
 
                 if ((I2C1STAT & _I2C1STAT_R_W_MASK) != 0U)
                 {
@@ -147,9 +154,9 @@ static void I2C1_TransferSM(void)
                     if ((I2C1STAT & _I2C1STAT_TBF_MASK) == 0U)
                     {
                         /* In the callback, slave must write to transmit register by calling I2Cx_WriteByte() */
-                        (void)i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_TX_READY, context);
+                        (void)i2c1SlaveObj.callback(I2C_SLAVE_TRANSFER_EVENT_TX_READY, context);
 
-                        sdaValue = (i2c1Obj.lastByteWritten & 0x80U);
+                        sdaValue = (i2c1SlaveObj.lastByteWritten & 0x80U);
                         I2C1_RiseAndSetupTime(sdaValue);
                     }
                 }
@@ -165,12 +172,12 @@ static void I2C1_TransferSM(void)
         {
             if (((I2C1STAT & (_I2C1STAT_TBF_MASK | _I2C1STAT_ACKSTAT_MASK))  == 0U))
             {
-                if (i2c1Obj.callback != NULL)
+                if (i2c1SlaveObj.callback != NULL)
                 {
                     /* I2C master wants to read. In the callback, slave must write to transmit register */
-                    (void)i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_TX_READY, context);
+                    (void)i2c1SlaveObj.callback(I2C_SLAVE_TRANSFER_EVENT_TX_READY, context);
 
-                    sdaValue = (i2c1Obj.lastByteWritten & 0x80U);
+                    sdaValue = (i2c1SlaveObj.lastByteWritten & 0x80U);
                 }
 
                 I2C1_RiseAndSetupTime(sdaValue);
@@ -184,10 +191,10 @@ static void I2C1_TransferSM(void)
         {
             if ((I2C1STAT & _I2C1STAT_RBF_MASK) != 0U)
             {
-                if (i2c1Obj.callback != NULL)
+                if (i2c1SlaveObj.callback != NULL)
                 {
                     /* I2C master wants to write. In the callback, slave must read data by calling I2Cx_ReadByte()  */
-                    (void)i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_RX_READY, context);
+                    (void)i2c1SlaveObj.callback(I2C_SLAVE_TRANSFER_EVENT_RX_READY, context);
                 }
                 /* Data read by the application; release the clock stretch */
                 I2C1CONSET = _I2C1CON_SCLREL_MASK;
@@ -200,8 +207,8 @@ void I2C1_CallbackRegister(I2C_SLAVE_CALLBACK callback, uintptr_t contextHandle)
 {
     if (callback != NULL)
     {
-        i2c1Obj.callback = callback;
-        i2c1Obj.context = contextHandle;
+        i2c1SlaveObj.callback = callback;
+        i2c1SlaveObj.context = contextHandle;
     }
 }
 
@@ -212,7 +219,10 @@ bool I2C1_IsBusy(void)
 
 uint8_t I2C1_ReadByte(void)
 {
-    return (uint8_t)I2C1RCV;
+    uint8_t readByte = (uint8_t)I2C1RCV;
+
+
+    return readByte;
 }
 
 void I2C1_WriteByte(uint8_t wrByte)
@@ -220,7 +230,7 @@ void I2C1_WriteByte(uint8_t wrByte)
     if ((I2C1STAT & _I2C1STAT_TBF_MASK)  == 0U)
     {
         I2C1TRN = wrByte;
-        i2c1Obj.lastByteWritten = wrByte;
+        i2c1SlaveObj.lastByteWritten = wrByte;
     }
 }
 
@@ -238,11 +248,12 @@ I2C_SLAVE_ERROR I2C1_ErrorGet(void)
 {
     I2C_SLAVE_ERROR error;
 
-    error = i2c1Obj.error;
-    i2c1Obj.error = I2C_SLAVE_ERROR_NONE;
+    error = i2c1SlaveObj.error;
+    i2c1SlaveObj.error = I2C_SLAVE_ERROR_NONE;
 
     return error;
 }
+
 
 void __attribute__((used)) I2C1_BUS_InterruptHandler(void)
 {
@@ -252,13 +263,13 @@ void __attribute__((used)) I2C1_BUS_InterruptHandler(void)
     /* ACK the bus interrupt */
     IFS2CLR = _IFS2_I2C1BCIF_MASK;
 
-    i2c1Obj.error = I2C_SLAVE_ERROR_BUS_COLLISION;
+    i2c1SlaveObj.error = I2C_SLAVE_ERROR_BUS_COLLISION;
 
-    if (i2c1Obj.callback != NULL)
+    if (i2c1SlaveObj.callback != NULL)
     {
-        uintptr_t context = i2c1Obj.context;
+        uintptr_t context = i2c1SlaveObj.context;
 
-        (void) i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_ERROR, context);
+        (void) i2c1SlaveObj.callback(I2C_SLAVE_TRANSFER_EVENT_ERROR, context);
     }
 }
 
