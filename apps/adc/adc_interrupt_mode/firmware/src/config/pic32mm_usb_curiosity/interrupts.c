c8797261d45d98cc2222730d48d3111ff4c5ef62
Pranjal Das <pranjal.das@microchip.com>
2024-10-23 14:39:06 +0530

    [ADC] Regenerated demo for adc__adc_interrupt_mode__firmware__pic32mm_usb_curiosity.X
    MH3-100658

--- apps/adc/adc_interrupt_mode/firmware/src/config/pic32mm_usb_curiosity/interrupts.c
+++ apps/adc/adc_interrupt_mode/firmware/src/config/pic32mm_usb_curiosity/interrupts.c
@@ -48,9 +48,9 @@
 // Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
-
 #include "interrupts.h"
 #include "definitions.h"
+
 
 
 // *****************************************************************************
@@ -74,7 +74,7 @@ void ADC_Handler (void);
 // Section: System Interrupt Vector definitions
 // *****************************************************************************
 // *****************************************************************************
-void __ISR(_ADC_VECTOR, ipl1SOFT) ADC_Handler (void)
+void __attribute__((used)) __ISR(_ADC_VECTOR, ipl1SOFT) ADC_Handler (void)
 {
     ADC_InterruptHandler();
 }
