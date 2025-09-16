a27a7a48db9e43112814dc24b06469c618095c8e
Syam Nair <Syam.Nair@microchip.com>
2025-09-16 15:25:37 +0000

    [APP] Regenerated demo for rcon__reset_handler__firmware
    MH3C-275

--- apps/rcon/reset_handler/firmware/src/config/pic32mm_usb_curiosity/initialization.c
+++ apps/rcon/reset_handler/firmware/src/config/pic32mm_usb_curiosity/initialization.c
@@ -15,7 +15,7 @@
 
 // DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -188,9 +188,9 @@ void SYS_Initialize ( void* data )
 
 	GPIO_Initialize();
 
-    CORETIMER_Initialize();
 	UART3_Initialize();
 
+    CORETIMER_Initialize();
 
     EVIC_Initialize();
 
